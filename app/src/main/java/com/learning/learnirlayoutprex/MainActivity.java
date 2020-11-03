9fb9ecfc79dfcda72bd5a9568fee18677cfcbe85
Hami Hafezi <dev.hamihafezi@gmail.com>
2020-11-03 15:25:05 +0330

    Code for MainActivity.java Create MainActivity2.java Lets S Another

--- app/src/main/java/com/learning/learnirlayoutprex/MainActivity.java
+++ app/src/main/java/com/learning/learnirlayoutprex/MainActivity.java
@@ -2,14 +2,33 @@ package com.learning.learnirlayoutprex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1to2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button1to2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intentFor1to2 = new Intent
+                        (MainActivity.this, MainActivity2.class);
+                startActivity(intentFor1to2);
+
+            }
+        });
+        infinitival();
+    }
+
+    private void infinitival() {
+        button1to2 = findViewById(R.id.buttonChangeActivity1to2);
     }
 }
